d8ca3c99014db120d6076721e507f5cb3e1b0684
duckatgit <duckatgit@gmail.com>
2024-10-30 02:18:42 +0530

    update API BASE_URL

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,5 @@
 'use client'
-import { useEffect, useState } from "react";
-import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table"
+import { useState } from "react";
 import { Button } from "@/components/ui/button"
 import {
   Dialog,
@@ -20,12 +11,10 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog"
 import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
 import axios from "axios";
 
 
 export default function Home() {
-  const [org, setOrg] = useState<any>([])
   const [createOrgDetails, setCreateOrgDetails] = useState({
     domain: "",
     organizationName: "",
@@ -33,55 +22,14 @@ export default function Home() {
     governmentIDFileLink: ""
   })
 
-  const getOrgList = async () => {
-    let config = {
-      headers: {
-        "Authorization": process.env.NEXT_PUBLIC_BEARER_TOKEN,
-      }
-    }
-    const getList = await axios.get(`https://api.wavital.ai/api/v1/user/organizations`, config)
-
-    if (getList.status == 200) {
-      setOrg(getList.data?.organizations)
-    }
-    else {
-      setOrg([])
-    }
-  }
-
-  const deleteOrg = async (id: string) => {
-    let config = {
-      headers: {
-        "Authorization": process.env.NEXT_PUBLIC_BEARER_TOKEN,
-      }
-    }
-    const getList = await axios.delete(`https://api.wavital.ai/api/v1/organization/${id}`, config)
-
-    if (getList.status == 200) {
-      getOrgList()
-    }
-  }
-
   const createOrg = async () => {
-    let config = {
-      headers: {
-        "Authorization": process.env.NEXT_PUBLIC_BEARER_TOKEN,
-      }
-    }
-    await axios.post(`https://api.wavital.ai/api/v1/organization`, {
+    await axios.post(`http://localhost:3001/users/register`, {
       domain: createOrgDetails.domain,
       organizationName: createOrgDetails.organizationName,
       organizationIDFileLink: createOrgDetails.organizationIDFileLink,
       governmentIDFileLink: createOrgDetails.governmentIDFileLink
-    }, config).then(() => {
-      getOrgList()
     })
   }
-
-  useEffect(() => {
-    getOrgList()
-  }, [])
-
 
   return (
     <div className="w-screen h-screen">
@@ -149,33 +97,6 @@ export default function Home() {
           </DialogContent>
         </Dialog>
       </div>
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead className="w-[100px]">id</TableHead>
-            <TableHead>Name</TableHead>
-            <TableHead>Action</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {org?.map((item: any) => {
-            return (
-              <TableRow>
-                <TableCell className="font-medium">{item?.id}</TableCell>
-                <TableCell>{item?.organizationName}</TableCell>
-                <TableCell>
-                  <button
-                    className="bg-red-200 py-2 px-10 rounded-md"
-                    onClick={() => {
-                      deleteOrg(item?.id)
-                    }}
-                  >Delete</button>
-                </TableCell>
-              </TableRow>
-            )
-          })}
-        </TableBody>
-      </Table>
     </div>
   );
 }
