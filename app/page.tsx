996825cc0a6332c09e34ec50e3457d9698178a3b
duckatgit <duckatgit@gmail.com>
2024-11-06 13:40:29 +0530

    updated with new API

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
 'use client'
-import { useState } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogFooter,
   DialogHeader,
   DialogTitle,
@@ -21,6 +28,7 @@ export default function Home() {
     organizationIDFileLink: "",
     governmentIDFileLink: ""
   })
+  const [analyticsData, setAnalyticsData] = useState<any>()
 
   const createOrg = async () => {
     await axios.post(`http://localhost:3001/users/register`, {
@@ -30,6 +38,18 @@ export default function Home() {
       governmentIDFileLink: createOrgDetails.governmentIDFileLink
     })
   }
+
+  const analytics = async () => {
+    await axios.get(`http://localhost:3001/pets/filterpet`).then((res) => {
+      console.log('res.data.result', res.data.result)
+      setAnalyticsData(res.data.result)
+    })
+  }
+
+  useEffect(() => {
+    analytics()
+  }, [])
+
 
   return (
     <div className="w-screen h-screen">
@@ -96,6 +116,70 @@ export default function Home() {
             </DialogFooter>
           </DialogContent>
         </Dialog>
+      </div>
+      <div className="w-full ml-4 flex gap-10 ">
+        <Card>
+          <CardHeader>
+            <CardTitle>ordered</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p>{analyticsData?.ordered}</p>
+          </CardContent>
+        </Card>
+
+        <Card>
+          <CardHeader>
+            <CardTitle>approved</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p>{analyticsData?.approved}</p>
+          </CardContent>
+        </Card>
+
+        <Card>
+          <CardHeader>
+            <CardTitle>placed</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p>{analyticsData?.placed}</p>
+          </CardContent>
+        </Card>
+
+        <Card>
+          <CardHeader>
+            <CardTitle>test</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p>{analyticsData?.test}</p>
+          </CardContent>
+        </Card>
+
+        <Card>
+          <CardHeader>
+            <CardTitle>pending</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p>{analyticsData?.pending}</p>
+          </CardContent>
+        </Card>
+
+        <Card>
+          <CardHeader>
+            <CardTitle>deliver</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p>{analyticsData?.deliver}</p>
+          </CardContent>
+        </Card>
+
+        <Card>
+          <CardHeader>
+            <CardTitle>delivered</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p>{analyticsData?.delivered}</p>
+          </CardContent>
+        </Card>
       </div>
     </div>
   );
